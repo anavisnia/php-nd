d98d2eb2df36546e1df356dbe37e97d5553aa0a9
Ana Višnevskaja <anavisnia@gmail.com>
2021-02-16 20:54:21 +0200

    antra uzduotis

--- masyvai2.php
+++ masyvai2.php
@@ -8,9 +8,25 @@ Sugeneruokite masyvą iš 10 elementų, kurio elementai būtų masyvai iš 5 ele
 */
 $multiArr = [];
 for ($i = 0; $i < 10; $i++) {
-    array_push($multiArr, [rand(0, 5), rand(0, 5), rand(0, 5), rand(0, 5), rand(0, 5)]);
+    array_push($multiArr, [rand(5, 25), rand(5, 25), rand(5, 25), rand(5, 25), rand(5, 25)]);
 }
 
 echo '<pre>';
 print_r($multiArr);
 echo '</pre>';
+
+//Antra
+echo '<br>' . '<h3>' . 'Antra Uzduotis' . '</h3>' . '<br>';
+/*
+Suskaičiuokite kiek masyve yra elementų didesnių už 10
+*/
+
+$numCount = 0;
+for ($i = 0, $j = 0; $i < count($multiArr); $i++, $j++) {
+    for ($j = 0; $j < count($multiArr[$i]); $j++) {
+        if ($multiArr[$i][$j] > 10) {
+            $numCount++;
+        }
+    }
+}
+echo $numCount;
