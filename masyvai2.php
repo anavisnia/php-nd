3fbf45e4cd11c79f5f9595cf2f607584568fc926
Ana Višnevskaja <anavisnia@gmail.com>
2021-02-18 19:50:17 +0200

    antra uzduotis D dalis

--- masyvai2.php
+++ masyvai2.php
@@ -17,22 +17,14 @@ foreach(range(1, 10) as $index1 => $_) {
         $multidArr[$index1][$index2] = rand(5, 25);
     }
 }
-
-_pc($multidArr);
-_pr($multidArr);
-//usort
-// a masyvas ir b masyvas sio atveju
-usort($multidArr, function($a, $b) {
-    return $b[1] <=> $a[1];
-});
 _pr($multidArr);
 
 //Antra
 echo '<br>' . '<h3>' . 'Antra Uzduotis' . '</h3>' . '<br>';
-/*
+
+/* *************************
 A:Suskaičiuokite kiek masyve yra elementų didesnių už 10
 */
-
 $count = 0;
 foreach($multidArr as $val1) {
     foreach ($val1 as $val2) {
@@ -43,7 +35,7 @@ foreach($multidArr as $val1) {
 }
 _pr("A: Didesniu uz 10 yra: $count skaiciai/skaicius.");
 
-/*
+/* *************************
 B: Raskite didžiausio elemento reikšmę;
 */
 $didziausiasSk = 0;
@@ -56,16 +48,24 @@ for ($i = 0; $i < count($multidArr); $i++) {
 }
 echo "B: Didziausiai skaicius: $didziausiasSk.  <br>";
 
-/*
+/* *************************
 C: Suskaičiuokite kiekvieno antro lygio masyvų su vienodais indeksais sumas (t.y. suma reikšmių turinčių indeksą 0, 1 ir t.t.)
 */
-//neveikai
+echo "Antra C neveikai <br>";
 $sum = 0;
-// for ($i = 0, $j = 0; $j < count($multidArr[$i]); $i++, $j++) {
-//     $sum = $multidArr[0][$j] + $multidArr[0][$j] + $multidArr[0][$j] + $multidArr[0][$j] + $multidArr[0][$j];
-//     echo $sum . '<br>';
+// foreach($multidArr as $key => $value) {
+
 // }
 
+/* *************************
+D: Visus masyvus “pailginkite” iki 7 elementų
+*/
+foreach ($multidArr as $key => $_) {
+    foreach(range(5, 7) as $val) {
+        $multidArr[$key][$val] = rand(5, 25);
+    }
+}
+_pr($multidArr);
 
 //Trecia
 echo '<br>' . '<h3>' . 'Trecia Uzduotis' . '</h3>' . '<br>';
