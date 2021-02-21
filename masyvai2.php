d4add5b9ce5a3d457839a03d41df03b853cd05b3
Ana Višnevskaja <anavisnia@gmail.com>
2021-02-21 13:40:54 +0200

    desimta random color

--- masyvai2.php
+++ masyvai2.php
@@ -239,3 +239,15 @@ Reikšmė value vienas iš atsitiktinai parinktų simbolių: #%+*@裡,
 o reikšmė color atsitiktinai sugeneruota spalva formatu: #XXXXXX. 
 Pasinaudoję masyvų atspausdinkite “kvadratą” kurį sudarytų masyvo reikšmės nuspalvintos spalva color.
 */
+$masyavs10 = [];
+$symbolArr = ['#', '%', '+', '*', '@', '裡'];
+for($i = 0; $i < 10; $i++) {
+    for($j = 0; $j < 10; $j++) {
+        $randColor = '';
+        while (strlen($randColor) != 6) {
+            $randColor .= rand(0, 9);
+        }
+        $masyavs10[$i][$j] = ['value' =>  '', 'color' => '#'.$randColor];
+    }
+}
+_pr($masyavs10);
