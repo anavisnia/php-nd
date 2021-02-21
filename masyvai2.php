e6433a7cf6d94acfd3d764d1114c7d90cc9ff5b0
Ana Višnevskaja <anavisnia@gmail.com>
2021-02-21 12:21:42 +0200

    desimta uzduotis aprasymas

--- masyvai2.php
+++ masyvai2.php
@@ -220,3 +220,13 @@ usort($masyvas9, function ($a, $b) {
     return $a <=> $b;
 });
 _pr($masyvas9);
+
+//Desimta
+echo '<br>' . '<h3>' . 'Desimta Uzduotis' . '</h3>' . '<br>';
+/*
+Sukurkite masyvą iš 10 elementų. 
+Jo reikšmės masyvai iš 10 elementų. Antro lygio masyvų reikšmės masyvai su dviem elementais value ir color. 
+Reikšmė value vienas iš atsitiktinai parinktų simbolių: #%+*@裡, 
+o reikšmė color atsitiktinai sugeneruota spalva formatu: #XXXXXX. 
+Pasinaudoję masyvų atspausdinkite “kvadratą” kurį sudarytų masyvo reikšmės nuspalvintos spalva color.
+*/
