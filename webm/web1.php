cb46c46ddea500194f746eacbc3c8f5973c445e0
Ana Višnevskaja <anavisnia@gmail.com>
2021-02-25 10:04:55 +0200

    name upd

--- webm/web1.php
+++ webm/web1.php
@@ -15,7 +15,7 @@ else {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>COLORADO</title>
+    <title>Nr1</title>
 </head>
 <body style="background:<?= $color ?>;">
 
