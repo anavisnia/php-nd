03f6b2950533e4bacf18b557dd1c9642d3701b54
Ana Višnevskaja <anavisnia@gmail.com>
2021-02-25 10:50:05 +0200

    lemon upd, redirect

--- webm/fruit/lemon.php
+++ webm/fruit/lemon.php
@@ -3,6 +3,7 @@
 Sukurkite du puslapius lemon.php ir orange.php. Jų fonus nuspalvinkite atitinkamom spalvom. Į lemon.php puslapį įdėkite kodą, kuris naršyklę visada peradresuotų į puslapį orange.php. Pademonstruokite veikimą.
 */
 header('location:http://localhost/bit/nd1/webm/fruit/orange.php');
+die; // uztvirtinam peradresavima
 ?>
 <!DOCTYPE html>
 <html lang="en">
