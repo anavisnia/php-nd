0ab8eaf7f6a5bb6ee18374c1bd1f46a4491ada20
Ana Višnevskaja <anavisnia@gmail.com>
2021-02-26 22:07:37 +0200

    9 uzduotis done

---
+++ webm/web9/rezult.php
@@ -0,0 +1,26 @@
+<?php 
+session_start();
+if(!empty($_POST) && $_SERVER['REQUEST_METHOD'] == 'POST') {
+    $postIlgis = count($_POST);
+    $_SESSION['rezultatas'] = $postIlgis; 
+    _pc($postIlgis);
+} else {
+    echo 'Klaida...';
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rezultatas</title>
+</head>
+<body>
+    <?php
+        if(!empty($_POST) && $_SERVER['REQUEST_METHOD'] == 'POST') {
+            echo '<p>Buvo pazymeta:' . ' ' . ($_SESSION['rezultatas']). ' ' . 'checkbox.</p>';
+        }
+    ?>
+</body>
+</html>
---
+++ webm/web9/web9v2.php
@@ -0,0 +1,35 @@
+<?php 
+/*
+Padarykite juodą puslapį, 
+kuriame būtų POST forma,
+ mygtukas 
+ ir atsitiktinis kiekis (3-10) checkbox su raidėm A,B,C… 
+Padarykite taip, kad paspaudus mygtuką, 
+fono spalva pasikeistų į baltą, 
+forma išnyktų ir atsirastų skaičius, 
+rodantis kiek buvo pažymėta checkboksų (ne kurie, o kiek).
+*/
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body style="background:<?= $color ?? 'black'?>">
+    <form style="color:white" action="./rezult.php" method="post">
+        <?php
+        $randNum = rand(3, 10);
+        $letetrs = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'];
+        for($i=0; $i < $randNum; $i++) {
+            echo '<input type="checkbox" name="check' . $letetrs[$i] . '">' . $letetrs[$i];
+        }
+        ?>
+        <button type="submit">Spausk</button>
+    </form>
+</body>
+</html>
