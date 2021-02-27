70fc86e78eb515fd0891dcef12fecfaa310045b9
Ana Višnevskaja <anavisnia@gmail.com>
2021-02-27 20:35:49 +0200

    button 'go back' added

--- webm/web9/rezult.php
+++ webm/web9/rezult.php
@@ -22,5 +22,6 @@ if(!empty($_POST) && $_SERVER['REQUEST_METHOD'] == 'POST') {
             echo '<p>Buvo pazymeta:' . ' ' . ($_SESSION['rezultatas']). ' ' . 'checkbox' . ' is ' .  $_SESSION['randNum'] . '.' .'</p>';
         }
     ?>
+    <a style="padding:8px 15px;background-color:#600080;color:white;text-decoration:none;border-radius:5px;" href="http://localhost/bit/nd1/webm/web9/web9v2.php">Go back</a>
 </body>
 </html>
