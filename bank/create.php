4022a07db5e11e84af5c1f6217cb996fc0072f24
Ana Višnevskaja <anavisnia@gmail.com>
2021-03-01 15:45:30 +0200

    main structure

--- bank/create.php
+++ bank/create.php
@@ -7,24 +7,34 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sukurimas saskaitos</title>
+    <title>Saskaitos Sukurimas</title>
 </head>
 <body>
-    <form action="" method="post">
-        <label for="fName">Vardas</label>
-        <input type="text" name="fName">
-        <br>
-        <label for="lName">Pavarde</label>
-        <input type="text" name="lName">
-        <br>
-        <label for="accountNum">Saskaitos Numeris</label>
-        <input type="text" name="accountNum">
-        <br>
-        <label for="personId">Asmens Kodas</label>
-        <input type="number" name="personId">
-        <br>
-        <button type="submit">Sukurti Saskaita</button>
-    </form>
-    
+    <div class="topnav">
+        <a class="active" href="index.php">Home</a>
+        <a href="create.php">Sukurti nauja saskaita</a>
+        <a href="add.php">Prideti lesas</a>
+        <a href="withdraw.php">Nuskaiciuoti lesas</a>
+    </div>
+    <section class="create_main">
+        <div class="person_info_header">
+            <h3>Sukurti Saskaita</h3>
+        </div>
+        <form action="" method="post">
+            <label for="fName">Vardas</label>
+            <input type="text" name="fName">
+            <br>
+            <label for="lName">Pavarde</label>
+            <input type="text" name="lName">
+            <br>
+            <label for="accountNum">Saskaitos Numeris</label>
+            <input type="text" name="accountNum">
+            <br>
+            <label for="personId">Asmens Kodas</label>
+            <input type="number" name="personId">
+            <br>
+            <button type="submit">Sukurti Saskaita</button>
+        </form>
+    </section>
 </body>
 </html>
---
+++ bank/withdraw.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nuskaiciuoti Lesas</title>
+</head>
+<body>
+    <div class="topnav">
+        <a class="active" href="index.php">Home</a>
+        <a href="create.php">Sukurti nauja saskaita</a>
+        <a href="add.php">Prideti lesas</a>
+        <a href="withdraw.php">Nuskaiciuoti lesas</a>
+    </div>
+    <section>
+        <div class="person_info_header">
+            <h3>Nuskaicioti Lesas</h3>
+        </div>
+        <div class="person_info_body">
+            <p>Kliento Vardas</p>
+            <p>Kliento Pavarde</p>
+            <p>Kliento Saskaitos Likutis:</p>
+            <form action="" method="post">
+                <label for="suma">Iveskite skaiciu kiek norite nuskaiciuoti:</label>
+                <input type="number" name="suma">
+            </form>
+        </div>
+    </section>
+    
+</body>
+</html>
