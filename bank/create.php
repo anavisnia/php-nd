595f217100c66491e40a5f9d393bf87721c5babc
Ana Višnevskaja <anavisnia@gmail.com>
2021-03-01 17:12:02 +0200

    create page css

--- bank/create.php
+++ bank/create.php
@@ -9,6 +9,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./css/reset.css">
     <link rel="stylesheet" href="./css/nav.css">
+    <link rel="stylesheet" href="./css/create.css">
     <title>Saskaitos Sukurimas</title>
 </head>
 <body>
@@ -22,21 +23,31 @@
         <div class="person_info_header">
             <h3>Sukurti Saskaita</h3>
         </div>
-        <form action="" method="post">
-            <label for="fName">Vardas</label>
-            <input type="text" name="fName">
-            <br>
-            <label for="lName">Pavarde</label>
-            <input type="text" name="lName">
-            <br>
-            <label for="accountNum">Saskaitos Numeris</label>
-            <input type="text" name="accountNum">
-            <br>
-            <label for="personId">Asmens Kodas</label>
-            <input type="number" name="personId">
-            <br>
-            <button type="submit">Sukurti Saskaita</button>
-        </form>
+        <div class="personal_info_body">
+            <form action="" method="post">
+                <div class="ul_item">
+                    <label style="color: cornflowerblue;" for="fName">Vardas</label>
+                    <input type="text" name="fName">
+                </div>
+                <br>
+                <div class="ul_item">
+                    <label style="color: cornflowerblue;" for="lName">Pavarde</label>
+                    <input type="text" name="lName">
+                </div>
+                <br>
+                <div class="ul_item">
+                    <label style="color: cornflowerblue;" for="accountNum">Saskaitos Numeris</label>
+                    <input type="text" name="accountNum">
+                </div>
+                <br>
+                <div class="ul_item">
+                    <label style="color: cornflowerblue;" for="personId">Asmens Kodas</label>
+                    <input type="number" name="personId">
+                </div>
+                <br>
+                <button type="submit">Sukurti Saskaita</button>
+            </form>
+        </div>
     </section>
 </body>
 </html>
