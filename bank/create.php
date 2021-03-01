987c31d9c2a48ef719ac645f427659c3a9c8e211
Ana Višnevskaja <anavisnia@gmail.com>
2021-03-01 14:55:08 +0200

    initial commit

---
+++ bank/create.php
@@ -0,0 +1,30 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sukurimas saskaitos</title>
+</head>
+<body>
+    <form action="" method="post">
+        <label for="fName">Vardas</label>
+        <input type="text" name="fName">
+        <br>
+        <label for="lName">Pavarde</label>
+        <input type="text" name="lName">
+        <br>
+        <label for="accountNum">Saskaitos Numeris</label>
+        <input type="text" name="accountNum">
+        <br>
+        <label for="personId">Asmens Kodas</label>
+        <input type="number" name="personId">
+        <br>
+        <button type="submit">Sukurti Saskaita</button>
+    </form>
+    
+</body>
+</html>
---
+++ bank/index.php
@@ -0,0 +1,47 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/reset.css">
+    <link rel="stylesheet" href="./css/main.css">
+    <title>Saskaita</title>
+</head>
+<body>
+    <div class="topnav">
+        <a class="active" href="index.php">Home</a>
+        <a href="create.php">Sukurti nauja saskaita</a>
+        <a href="add.php">Prideti lesas</a>
+        <a href="withdraw.php">Nuskaiciuoti lesas</a>
+    </div>
+    <section class="hero">
+        <h1>Bankas</h1>
+    </section>
+    <section class="saskaitu_sarasas">
+        <div class="sarasas">
+        <h3>Saskaitu sarasa:</h3>
+        <ul>
+            <li>Saskaita 1</li>
+            <button type="submit">Istrinti</button>
+            <a href="#">Prideti</a>
+            <a href="#">Nuskaiciuoti</a>
+
+            <li>Saskaita 2</li>
+            <button type="submit">Istrinti</button>
+            <a href="#">Prideti</a>
+            <a href="#">Nuskaiciuoti</a>
+
+            <li>Saskaita 3</li>
+            <button type="submit">istrinti</button>
+            <a href="#">Prideti</a>
+            <a href="#">Nuskaiciuoti</a>
+        </ul>
+        </div>
+    </section>
+    
+</body>
+</html>
