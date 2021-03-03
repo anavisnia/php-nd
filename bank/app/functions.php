3671e1d56413625c8effb122e3e1ea2507d34726
Ana Višnevskaja <anavisnia@gmail.com>
2021-03-03 20:24:04 +0200

    write data + get index of array

--- bank/app/functions.php
+++ bank/app/functions.php
@@ -9,3 +9,37 @@ function readData() : array //funkcija privalo grazinti array
     $data = file_get_contents(DIR.'data/users.json');
     return json_decode($data, 1);
 }
+
+function writeData(array $data) : void
+{
+    $data = json_encode($data); // is stringo i masyva
+    file_put_contents(DIR.'data/users.json', $data);
+}
+
+function getNextId() : int
+{
+    if(!file_exists(DIR.'data/indexes.json')) {
+        $index = json_encode(['id' => 1]);
+        file_put_contents(DIR.'data/indexes.json', $index);
+    }
+    $index = file_get_contents(DIR.'data/indexes.json');
+    $id = (int) $index['id'];
+    $index['id'] = $id + 1;
+    file_put_contents(DIR.'/data/indexes.json', $index);
+    return $id;
+}
+
+
+
+
+
+
+
+
+/*
+//  accountNum - LT + 18num    personId - 11num
+[
+    ['id' => '1', 'name' => 'A', 'surname' => 'B', 'accountNum' => 'LT123456789012345678', 'personId' => '13793847258'],
+    ['id' => '2', 'name' => 'C', 'surname' => 'D', 'accountNum' => 'LT123456789012345678', 'personId' => '13793847258'],
+]
+*/
--- bank/data/data.php
+++ bank/data/data.php
@@ -1,6 +1,6 @@
 <?php
-$users = [
-    ['fName' => '', 'lName' => '', 'accountNum' => '', 'personId' => ''],
-];
+// $users = [
+//     ['fName' => '', 'lName' => '', 'accountNum' => '', 'personId' => ''],
+// ];
 
-file_put_contents('data/users.json', json_encode($users));
+// file_put_contents('data/users.json', json_encode($users));
