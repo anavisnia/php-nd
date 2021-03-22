d7bdd6f5c63018b3f74366b034370521a162ec85
Ana Višnevskaja <anavisnia@gmail.com>
2021-03-22 16:52:41 +0200

    variable type fix

--- nd-laravel/app/Http/Controllers/CalcController.php
+++ nd-laravel/app/Http/Controllers/CalcController.php
@@ -6,13 +6,13 @@ use Illuminate\Http\Request;
 
 class CalcController extends Controller
 {
-    public function add($num1, $num2)
+    public function add(float $num1, float $num2)
     {
         $answer = $num1 + $num2;
         return view('calc.add',['firstNum' => $num1, 'secNum' => $num2, 'answer' => $answer]);
     }
     
-    public function div($num1, $num2)
+    public function div(float $num1, float $num2)
     {
         if($num1 == 0 || $num2 == 0) {
             $answer = 'Devision by zero is unacceptable!';
@@ -23,19 +23,19 @@ class CalcController extends Controller
         return view('calc.div',['firstNum' => $num1, 'secNum' => $num2, 'answer' => $answer]);
     }
 
-    public function sub($num1, $num2)
+    public function sub(float $num1, float $num2)
     {
         $answer = $num1 - $num2;
         return view('calc.sub',['firstNum' => $num1, 'secNum' => $num2, 'answer' => $answer]);
     }
 
-    public function mult($num1, $num2)
+    public function mult(float $num1, float $num2)
     {
         $answer = $num1 * $num2;
         return view('calc.mult',['firstNum' => $num1, 'secNum' => $num2, 'answer' => $answer]);
     }
 
-    public function mod($num1, $num2)
+    public function mod(float $num1, float $num2)
     {
         if($num1 == 0 || $num2 == 0) {
             $answer = 'Modulo by zero is unacceptable!';
