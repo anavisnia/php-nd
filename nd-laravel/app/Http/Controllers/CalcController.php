d2c502668dcac80f6e135ebbe4282def52fdfe5a
Ana Višnevskaja <anavisnia@gmail.com>
2021-03-22 16:40:38 +0200

    rewrited calculator

--- nd-laravel/app/Http/Controllers/CalcController.php
+++ nd-laravel/app/Http/Controllers/CalcController.php
@@ -6,35 +6,49 @@ use Illuminate\Http\Request;
 
 class CalcController extends Controller
 {
-    public function index(string $operation, int $num1, int $num2)
+    public function add($num1, $num2)
     {
-        if($operation === 'mult') {
-            $operation = 'multiply';
-            $answer = $num1 * $num2;
-        } elseif($operation === 'add') {
-            $operation = 'addition';
-            $answer = $num1 + $num2;
-        } elseif($operation === 'sub') {
-            $operation = 'substract';
-            $answer = $num1 - $num2;
-        } elseif($operation === 'div') {
-            $operation = 'division';
-            if($num1 === 0 || $num2 === 0) {
-                $answer = 'Devision by zero is unacceptable!';
-            } else {
-                $answer = $num1 / $num2;
-                $answer = round($answer, 2);
-            }
-        } elseif($operation === 'mod') {
-            $operation = 'reminder of devision';
-            if($num1 === 0 || $num2 === 0) {
-                $answer = 'Modulo by zero is unacceptable!';
-            } else {
-                $answer = $num1 % $num2;
-                $answer = round($answer, 2);
-            }
+        $operation = 'addition';
+        $answer = $num1 + $num2;
+        return view('calc.add',['op' => $operation, 'firstNum' => $num1, 'secNum' => $num2, 'answer' => $answer]);
+    }
+    
+    public function div($num1, $num2)
+    {
+        $operation = 'division';
+        if($num1 === 0 || $num2 === 0) {
+            $answer = 'Devision by zero is unacceptable!';
+        } else {
+            $answer = $num1 / $num2;
+            $answer = round($answer, 2);
         }
-        return view('calc.calculate',['op' => $operation, 'firstNum' => $num1, 'secNum' => $num2, 'answer' => $answer]);
+        return view('calc.div',['op' => $operation, 'firstNum' => $num1, 'secNum' => $num2, 'answer' => $answer]);
+    }
+
+    public function sub($num1, $num2)
+    {
+        $operation = 'substract';
+        $answer = $num1 - $num2;
+        return view('calc.sub',['op' => $operation, 'firstNum' => $num1, 'secNum' => $num2, 'answer' => $answer]);
+    }
+
+    public function mult($num1, $num2)
+    {
+        $operation = 'multiply';
+        $answer = $num1 * $num2;
+        return view('calc.mult',['op' => $operation, 'firstNum' => $num1, 'secNum' => $num2, 'answer' => $answer]);
+    }
+
+    public function mod($num1, $num2)
+    {
+        $operation = 'reminder of devision';
+        if($num1 === 0 || $num2 === 0) {
+            $answer = 'Modulo by zero is unacceptable!';
+        } else {
+            $answer = $num1 % $num2;
+            $answer = round($answer, 2);
+        }
+        return view('calc.mod',['op' => $operation, 'firstNum' => $num1, 'secNum' => $num2, 'answer' => $answer]);
     }
 
 }
---
+++ nd-laravel/resources/views/calc/sub.blade.php
@@ -0,0 +1,10 @@
+@extends('layout.main')
+@section('title') Calculator V2 @endsection
+@section('hero')
+<h1 class="hero">Let's calculate!</h1>
+<div class="hero">
+    <p>Your chosen method: substraction</p>
+    <p>Your chosen numbers are <span class="numColor">{{ $firstNum }}</span> and <span class="numColor">{{$secNum}}</span></p>
+    <p>Answer: <span class="numColor">{{$answer}}</span></p>
+</div>
+@endsection
